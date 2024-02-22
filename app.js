d5bfb1c71884db34640a0630bc2030db08455b4c
syedubaidshah <obaidshah0099@gmail.com>
2024-02-23 00:36:49 +0500

    js_1st_assigment

---
+++ app.js
@@ -0,0 +1,26 @@
+let myname = "obaid";
+console.log("my name is " + myname);
+
+console.log("Add:", 9 + 6);
+console.log("Sub:", 9 - 6);
+console.log("multi:", 9 * 6);
+console.log("divi:", 9 / 6);
+
+// **************
+
+let yourName = prompt("Enter your name");
+alert("Welcome " + yourName);
+
+let num1 = prompt("First Number");
+let num2 = prompt("Last Number");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+// console.log("num1: ", num1);
+// console.log("num2: ", num2);
+
+console.log("ADD: ", num1 + num2);
+console.log("SUBTRACT:", num1 - num2);
+console.log("DIVISION:", num1 / num2);
+console.log("MUlTIPLICATION:", num1 * num2);
