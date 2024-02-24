09e9e8ed47f3b8a228feeb780b2fecfee84e7d23
Waleed Shoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-02-24 11:42:30 +0500

    changes

--- app.js
+++ app.js
@@ -17,26 +17,14 @@ let num2 = prompt("Last Number");
 num1 = Number(num1);
 num2 = Number(num2);
 
-// console.log("num1: ", num1);
-// console.log("num2: ", num2);
+console.log("num1: ", num1);
+console.log("num2: ", num2);
 
-console.log("ADD: ", num1 + num2);
-console.log("SUBTRACT:", num1 - num2);
-console.log("DIVISION:", num1 / num2);
-console.log("MUlTIPLICATION:", num1 * num2);
+let Add = num1 + num2;
+let Sub = num1 - num2;
 
+alert("Additon:" + Add )
+alert("SUBTRACT:" + Sub )
+alert("DIVISION:" + num1 * num2 )
+alert("MUlTIPLICATION:" + num1 / num2 )
 
-// let Add = a + b;
-// let Sub = a - b;
-// let Mul = a * b;
-// let Div = a / b;
-
-// alert("Sum of a, b, is equal " + Add);
-// alert("Sub of a, b, is equal " + Sub);
-// alert("Mul of a, b, is equal " + Mul);
-// alert("Div of a, b, is equal " + Div);
-
-// console.log("Sum of a, b, is equal " + Add);
-// console.log("Sum of a, b, is equal " + Sub);
-// console.log("Mul of a, b, is equal " + Mul);
-// console.log("Div of a, b, is equal " + Div);
