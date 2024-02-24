c3a2e6e8e72e799d091695a5655f253535af5b0b
syedubaidshah <obaidshah0099@gmail.com>
2024-02-24 09:27:17 +0500

    change

--- app.js
+++ app.js
@@ -24,3 +24,19 @@ console.log("ADD: ", num1 + num2);
 console.log("SUBTRACT:", num1 - num2);
 console.log("DIVISION:", num1 / num2);
 console.log("MUlTIPLICATION:", num1 * num2);
+
+
+// let Add = a + b;
+// let Sub = a - b;
+// let Mul = a * b;
+// let Div = a / b;
+
+// alert("Sum of a, b, is equal " + Add);
+// alert("Sub of a, b, is equal " + Sub);
+// alert("Mul of a, b, is equal " + Mul);
+// alert("Div of a, b, is equal " + Div);
+
+// console.log("Sum of a, b, is equal " + Add);
+// console.log("Sum of a, b, is equal " + Sub);
+// console.log("Mul of a, b, is equal " + Mul);
+// console.log("Div of a, b, is equal " + Div);
